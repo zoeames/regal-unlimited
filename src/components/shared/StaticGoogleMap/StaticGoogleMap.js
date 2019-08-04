c2310f7428de4056d2efc4aeab2a970223466451
Zoe Ames <zoeames@gmail.com>
2019-08-04 10:30:36 -0500

    static map works

---
+++ src/components/shared/StaticGoogleMap/StaticGoogleMap.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import apiKeys from '../../../helpers/apiKeys.json';
+
+import './StaticGoogleMap.scss';
+
+const googleApiKey = apiKeys.firebaseKeys.apiKey;
+
+
+class StaticGoogleMap extends React.Component {
+  googleMapRef = React.createRef()
+
+  componentDidMount() {
+    const googleMapScript = document.createElement('script');
+    googleMapScript.src = `https://maps.googleapis.com/maps/api/js?key=${googleApiKey}&libraries=places`;
+    window.document.body.appendChild(googleMapScript);
+
+    googleMapScript.addEventListener('load', () => {
+      this.googleMap = this.createGoogleMap(this.props.coords);
+      this.marker = this.createMarker(this.props.coords);
+    });
+  }
+
+  createGoogleMap = coords => new window.google.maps.Map(this.googleMapRef.current, {
+    zoom: 16,
+    center: {
+      lat: coords.lat,
+      lng: coords.lng,
+    },
+    disableDefaultUI: true,
+  })
+
+  createMarker = coords => new window.google.maps.Marker({
+    position: coords,
+    map: this.googleMap,
+  })
+
+  render() {
+    return (
+      <div
+        id="google-map"
+        ref={this.googleMapRef}
+      />
+    );
+  }
+}
+
+export default StaticGoogleMap;
